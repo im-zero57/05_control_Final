2798c6026e0e4f4e050d32fb3ca797b731a5a9dc
im-zero57 <leman000@sookmyung.ac.kr>
2019-10-10 23:55:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num;
-	char i;
+	int answer;
+	int t;
 	
-	printf("문장을 입력해주세요 : ");
-	while( (i=getchar() ) !='\n')
-	{if (i<='9'&& i>='0')
-		num++;
+	do
+	{
+		printf("숫자를 입력해주세요 : ");
+		scanf("%d",&answer);
+		if (answer > 59)
+			printf("low!\n");
+		else if (answer < 59)
+			printf("high!\n");
+		t++;		
 	}
 	
-	printf("축하합니다 당신이 입력한 문장 속 숫자의 개수는 %d개 입니다.\n",num);
-	
+	while (answer!=59);
+	printf("축하합니다! 정답을 맞추셨습니다! 정답을 맞추기 위한 총 시도횟수는 %d회 입니다.\n",t);
 	return 0;
 }
