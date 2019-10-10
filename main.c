03a035e20abc48e962a4be727cd0377a10bdde41
im-zero57 <leman000@sookmyung.ac.kr>
2019-10-10 23:47:32 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	for(i=0;i<10;i++)
-		printf("Hello World!\n");
+	int num;
+	char i;
+	
+	printf("문장을 입력해주세요 : ");
+	while( (i=getchar() ) !='\n')
+	{if (i<='9'&& i>='0')
+		num++;
+	}
+	
+	printf("축하합니다 당신이 입력한 문장 속 숫자의 개수는 %d개 입니다.\n",num);
 	
 	return 0;
 }
